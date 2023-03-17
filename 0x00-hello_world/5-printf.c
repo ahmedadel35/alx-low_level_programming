451db385a3c47788ae5a930a225c7d4c86ebfa9d
abo3adel <abo3adel35@gmail.com>
2023-03-17 23:01:55 +0200

    clang

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,15 @@
-#include <stdio.h>  
+#include <stdio.h>
 
-int main() {  
-    printf("with proper grammar, but the outcome is a piece of art,");  
-    return 0;  
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
-int main(void) {
-    char charType;
-    int intType;
-    long longIntType;
-    double doubleType;
-    float floatType;
 
-    // sizeof evaluates the size of a variable
-    printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-    printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-    printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
-    printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
-    printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long longIntType;
+	double doubleType;
+	float floatType;
 
-    return 0;
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+
+	return (0);
 }
