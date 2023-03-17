eb528209a1ea4d74b05096cdb99e460431ff2850
abo3adel <abo3adel35@gmail.com>
2023-03-17 23:12:04 +0200

    clang

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		putchar(str[i])
+	}
+
+	putchar("\n");
+
+	return (1);
+}
