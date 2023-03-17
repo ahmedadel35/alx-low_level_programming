db983c72784834764ad9ca657afeaa03fee4b282
abo3adel <abo3adel35@gmail.com>
2023-03-17 23:15:49 +0200

    clang

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - Entry point
@@ -13,11 +14,9 @@ int main(void)
 	int i = 0;
 
 	for (i = 0; i < strlen(str); i++)
-	{
-		putchar(str[i])
-	}
+		putchar(str[i]);
 
-	putchar("\n");
+	putchar('\n');
 
 	return (1);
 }
