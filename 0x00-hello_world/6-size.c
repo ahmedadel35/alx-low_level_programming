65d6d2f9a16abb8c3fb40c559b9f6893ff15d6f0
abo3adel <abo3adel35@gmail.com>
2023-03-17 22:49:13 +0200

    clang

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    char charType;
+    int intType;
+    long longIntType;
+    double doubleType;
+    float floatType;
+
+    // sizeof evaluates the size of a variable
+    printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+    printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+    printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+    printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
+    printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+
+    return 0;
+}
