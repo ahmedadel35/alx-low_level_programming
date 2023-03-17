6be92d271c19bac7ecaca9851163eb9695238c59
abo3adel <abo3adel35@gmail.com>
2023-03-18 01:23:13 +0200

    nums

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				if (i == j || i == k || j == k)
+				{
+					continue;
+				}
+				if ((i * 100 + j * 10 + k) > (k * 100 + j * 10 + i))
+				{
+					continue;
+				}
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
