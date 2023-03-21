b16c57946066d37bca641f6af1997571fc504e2a
abo3adel <abo3adel35@gmail.com>
2023-03-22 01:12:15 +0200

    some

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,11 @@
 #include "main.h"
 
 /**
+ * add - Add two integers and return the result.
+ * @a: The first integer to be added.
+ * @b: The second integer to be added.
  *
+ * Return: The sum of the two integers.
  */
 void print_to_98(int n)
 {
