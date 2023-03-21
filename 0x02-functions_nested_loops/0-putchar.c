8be7f7875ab45492e37c8e61c199c248ad9f9edc
abo3adel <abo3adel35@gmail.com>
2023-03-21 23:41:57 +0200

    put

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
