41ecfa34cf151d11a23967d34a32c516f05d4bfb
abo3adel <abo3adel35@gmail.com>
2023-03-22 00:25:37 +0200

    alph

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,14 @@
  */
 int _putchar(char c);
 
+/**
+ * print_alphabet - Entry point
+ *
+ * Definition: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+
+
 #endif /* MAIN_H */
