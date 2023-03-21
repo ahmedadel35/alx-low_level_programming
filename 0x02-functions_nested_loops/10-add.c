a5a27f9b37d0b863a5e00e06234c40de01a13390
abo3adel <abo3adel35@gmail.com>
2023-03-22 01:08:04 +0200

    some

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Add two integers and return the result.
+ * @a: The first integer to be added.
+ * @b: The second integer to be added.
+ *
+ * Return: The sum of the two integers.
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return sum;
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h";
+
+/**
+ *
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (int i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (int i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
