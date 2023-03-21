dedabbffc7a97e3fbb949d157f6f742137688753
abo3adel <abo3adel35@gmail.com>
2023-03-22 00:12:46 +0200

    char

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Entry point
+ *
+ * Definition: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(char c);
+
+#endif /* MAIN_H */
