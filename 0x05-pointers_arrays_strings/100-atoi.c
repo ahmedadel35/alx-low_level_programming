caf4a5b8b33b40f056a3fd6c2d293a6087813e16
abo3adel <abo3adel35@gmail.com>
2023-03-28 00:43:58 +0200

    ato

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,7 +5,7 @@
  *
  * Description: 'the program's description'
  *
- * @s: the
+ * @s: we
  * Return: desr
  */
 int _atoi(char *s)
@@ -14,7 +14,6 @@ int _atoi(char *s)
 	int sign = 1;
 	int i = 0;
 
-	/* Check for sign at beginning of string */
 	if (s[0] == '-')
 	{
 		sign = -1;
@@ -25,12 +24,22 @@ int _atoi(char *s)
 		i++;
 	}
 
-	/* Convert each digit to integer and add to result */
 	while (s[i] != '\0')
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			result = result * 10 + (s[i] - '0');
+		}
+		else if (s[i] == '-' || s[i] == '+')
+		{
+			if (sign == 0)
+			{
+				sign = (s[i] == '-') ? -1 : 1;
+			}
+			else
+			{
+				break;
+			}
 		}
 		else
 		{
