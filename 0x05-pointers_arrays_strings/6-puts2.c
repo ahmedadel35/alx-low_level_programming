2a04706cd292b3ee0bfeaa3b7a0e30595a9a5a41
abo3adel <abo3adel35@gmail.com>
2023-03-28 00:31:09 +0200

    put2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * puts2 - check the code
+ *
+ * Description: 'the program's description'
+ *
+ * @str: the
+ */
 void puts2(char *str)
 {
+	int i;
 	int length = strlen(str);
 
-	for (int i = 0; i < length; i += 2)
+	for (i = 0; i < length; i += 2)
 	{
 		printf("%c", str[i]);
 	}
