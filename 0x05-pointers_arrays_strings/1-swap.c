37b8b2d987485c54923ca618ad8d8b0e495f909d
abo3adel <abo3adel35@gmail.com>
2023-03-27 23:58:53 +0200

    swap_int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * swap_int - check the code
+ *
+ * Description: 'the program's description'
+ *
+ * @a: the
+ * @b: the
+ */
+void swap_int(int *a, int *b)
+{
+	int swap;
+	int *ptr;
+
+	ptr = &swap;
+
+	*ptr = a;
+	*a = b;
+	*b = swap;
+}
