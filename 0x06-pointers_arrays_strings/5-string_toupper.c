efa2287cf8f53ec41d3c231164992d5df783e3bd
abo3adel <abo3adel35@gmail.com>
2023-03-31 17:20:14 +0200

    main

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * string_toupper - up to n characters from
+ * src string to the end of the dest string.
+ * Adds a null terminator to the end of dest.
+ *
+ * @str: asd
+ * Returns a pointer to the start of dest.
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		str[i] = toupper(str[i]);
+		i++;
+	}
+	return (str);
+}
