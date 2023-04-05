2e530e5cc3d9d83f54de1aa263e531e6ca59045c
abo3adel <abo3adel35@gmail.com>
2023-04-06 01:23:11 +0200

    wild

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,12 +1,12 @@
 /**
- * wildcmp_helper - Compares two strings and returns 1 if they are identical,
+ * wildcmp - Compares two strings and returns 1 if they are identical,
  * 0 otherwise
  * @s1: The first string to compare
  * @s2: The second string to compare
  *
  * Return: 1 if the strings are identical, 0 otherwise
  */
-int wildcmp_helper(char *s1, char *s2)
+int wildcmp(char *s1, char *s2)
 {
 	if (*s1 != '\0' && *s2 != '\0')
 	{
@@ -35,17 +35,4 @@ int wildcmp_helper(char *s1, char *s2)
 	{
 		return (0);
 	}
-}
-
-/**
- * wildcmp - Compares two strings and returns 1 if they are identical,
- * 0 otherwise
- * @s1: The first string to compare
- * @s2: The second string to compare
- *
- * Return: 1 if the strings are identical, 0 otherwise
- */
-int wildcmp(char *s1, char *s2)
-{
-	return (wildcmp_helper(s1, s2));
 }
