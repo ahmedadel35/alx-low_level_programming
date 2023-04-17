2439e1791d7399106bf2c92cea3b87f17d1d207e
abo3adel <abo3adel35@gmail.com>
2023-04-17 21:26:03 +0200

    dogy

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,10 +8,9 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d != NULL) /* check if pointer is not NULL */
-	{
-		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
-	}
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
